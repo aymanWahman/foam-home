e4a8807b9cc0bd49f9b2ee408567edb88e8794bf
AymanAly <101224684+aymanWahman@users.noreply.github.com>
2024-04-22 01:56:53 -0700

    price

--- src/components/dataProducts.js
+++ src/components/dataProducts.js
@@ -4,51 +4,51 @@ const DataProducts = [
     "id":1,
     "title": "كوباية كرتون  ",
     "image": "/img/cupK.jpg",
-    "description": " كوب كرتون 7 أونز بدون غطاء",
-    "price": 1.25
+    "description": "   كوب كرتون 7 أونز بدون غطاء وأقل كمية 50 كوب",
+    "price": 0.75
   },
   {
     "id":2,
     "title": "  كوباية عصائر",
     "image": "/img/cupPG.jpg",
-    "description": " كوب عصير بلاستك شفاف 12 أونز بدون غطاء",
-    "price": 2.25
+    "description": "  كوب عصير بلاستك شفاف 12 أونز بدون غطاء وأقل كمية 50 كوب",
+    "price": 1.5
   },{
     "id":3,
     "title": "  كوباية مياه",
     "image": "/img/cupP.jpg",
-    "description": " كوب مياه شفاف 250 ملي ",
-    "price": 1.25
+    "description": "  كوب مياه شفاف 250 ملي وأقل كمية 50 كوب ",
+    "price": 0.75
   },{
     "id":4,
     "title": "علبة صوص",
     "image": "/img/sauceG.jpg",
     "description": "علبة صوص بالغطاء 60 جرام ",
-    "price": 1.75
+    "price": 1.25
   },{
     "id":5,
     "title": " علبة وجبات  ",
     "image": "/img/meal2.jpg",
     "description": "علبة وجبات بالغطاء 1 و 2 عين ",
-    "price": 12
+    "price": 7.5
   },{
     "id":6,
     "title": "علبة مستطيل",
     "image": "/img/mostatel1000.jpg",
     "description": "علبة مستطيل شفاف 8 أونز بالغطاء ",
-    "price": 4.25
+    "price": 3
   },{
     "id":7,
     "title": "طبق مفصلي",
     "image": "/img/dishG.jpg",
     "description": " طبق متعدد الاغراض 250 ملي ",
-    "price": 3.25
+    "price": 2
   },{
     "id":8,
     "title": "طبق ريزو ",
     "image": "/img/rezo2.jpg",
     "description": " طبق قاع ريزو ميكرويف بالغطاء",
-    "price": 6.75
+    "price": 4.25
   },
   {
     "id":9,
@@ -68,15 +68,15 @@ const DataProducts = [
     "id":11,
     "title": " ملعقة",
     "image": "/img/spoon.jpg",
-    "description": "ملعقة اسود خامة ممتازة",
-    "price": 1
+    "description": "ملعقة اسود خامة ممتازة وأقل كمية 50 ملعقة",
+    "price": 0.75
   }
   // {
   //   "id":11,
   //   "title": "علبة ايس كريم",
   //   "image": "/img/ice.jpg",
   //   "description": "علبة ايس كريم 200 ملي بالغطاء",
-  //   "price": 2.5
+  //   "price": 2.25
   // }
 ]
 
