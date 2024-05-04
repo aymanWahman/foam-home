5d7ba371321fa068bd47238e2d3d84ca2e027cea
AymanAly <101224684+aymanWahman@users.noreply.github.com>
2024-05-03 19:12:37 -0700

    prise

--- src/components/dataProducts.js
+++ src/components/dataProducts.js
@@ -90,28 +90,35 @@ const DataProducts = [
     "title": "علبة مربع مفصلي",
     "image": "/img/morbaa4.jpeg",
     "description": "علبة مربع مفصلي 3 أونز",
-    "price": 2.25
+    "price": 1.5
   },
   {
     "id":15,
     "title": "علبة مربع مفصلي ",
     "image": "/img/mor.jpeg",
     "description": "علبة مربع مفصلي 4 أونز",
-    "price": 2.25
+    "price": 1.5
   },
   {
     "id":16,
     "title": "علبة سلطة",
     "image": "/img/sauceR.jpg",
-    "description": "علبة سلطة دائري 80 ملي  ",
-    "price": 2.25
+    "description": "علبة سلطة بيضاوي 80 ملي  ",
+    "price": 1.25
   },
   {
     "id":17,
     "title": "علبة وجبات ",
     "image": "/img/m.jpeg",
     "description": "علبة وجبات بالغطاء واحد عين",
-    "price": 7
+    "price": 7.5
+  },
+  {
+    "id":18,
+    "title": "طبق مسطح ",
+    "image": "/img/mostah.jpeg",
+    "description": "طبق مسطح شفاف 1/8 كيلو  ",
+    "price": 1
   }
 ]
 
